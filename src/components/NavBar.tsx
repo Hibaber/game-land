f469974f63f3e156ce873080e7fad2b0e31a4da1
Hibaber <hiba.berber@gmail.com>
2023-04-23 15:45:49 +0200

    implement search

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,11 +2,16 @@ import { HStack, Image } from "@chakra-ui/react";
 import ColorModeSwitch from "./ColorModeSwitch";
 import logo from "../assets/logo.webp";
 import SeacrhInput from "./SeacrhInput";
-const NavBar = () => {
+
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+const NavBar = ({ onSearch }: Props) => {
   return (
     <HStack padding="10px">
       <Image src={logo} boxSize="60px" />
-      <SeacrhInput />
+      <SeacrhInput onSearch={onSearch} />
       <ColorModeSwitch />
     </HStack>
   );
